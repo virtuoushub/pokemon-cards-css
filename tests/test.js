80ed5767613908e6b16298befa85aaa6affef334
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-10-29 14:14:03 -0400

    test(e2e): configure and add more playwright tests

--- tests/test.js
+++ tests/test.js
@@ -2,5 +2,19 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+	expect(await page.textContent('h1')).toBe('Pokemon Cards V2');
+});
+
+test('index page has expected first h2', async ({ page }) => {
+	await page.goto('/');
+	expect(await page.textContent('h2')).toBe(
+		'(Go ahead, try clicking a card to take a closer look!)'
+	);
+});
+
+test('index page has all expected h2', async ({ page }) => {
+	await page.goto('/');
+	const elements = page.locator('h2');
+	const isExpectedH2Count = await elements.evaluateAll((h2s, count) => h2s.length === count, 16);
+	expect(isExpectedH2Count).toBeTruthy();
 });
