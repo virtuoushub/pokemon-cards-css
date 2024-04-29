24fffc0b19b0bd5dd9cd1836d8d200a379a7e72e
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2024-04-29 08:42:28 -0400

    chore(eslint): fix config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,6 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'prettier'],
-	plugins: ['svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	extends: ['plugin:svelte/recommended','eslint:recommended', 'prettier'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
